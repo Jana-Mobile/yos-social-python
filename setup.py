60b17bb85ea2d3986888c5d7524c06c8e2ece8f4
Jeff Frasca <jeff@jana.com>
2014-01-06 13:42:02 -0800

    Fix setup.py to use requirements instead of copied source trees and bump version

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ from distutils.core import setup
 
 setup(
     name='yos_social_sdk',
-    version='0.0.2',
+    version='0.0.3',
     description='Python client library for Yahoo! Social + Data REST APIs',
     author='Dustin Whittle',
     author_email='dustin@yahoo-inc.com',
     license='MIT',
     url='http://github.com/yahoo/yos-social-python/tree/master',
-    packages=['yahoo', 'oauthlib', 'openid', 'simplejson'],
-    package_dir={'yahoo': 'src/yahoo', 'oauthlib': 'src/oauthlib', 'openid': 'src/openid', 'simplejson': 'src/simplejson'}
+    packages=['yahoo','yahoo.oauthlib'],
+    package_dir={'yahoo': 'src/yahoo'},
+    install_requires=[
+        'simplejson >= 1.3',
+        'python-openid >= 2.2.1'
+    ]
 )
